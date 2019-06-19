af95da2f49c555cba3c45dd28840da1dee79bfa6
manish2981 <manish29malhotra@gmail.com>
2019-06-20 01:05:38 +1000

    Added more test cases. Added New test class

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace GameEngine.Tests
@@ -12,5 +11,81 @@ namespace GameEngine.Tests
             Assert.True(sut.IsNoob);
 
         }
+        [Fact]
+        public void CalculateFullName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+            Assert.Equal("Sarah Smith", sut.FullName); 
+        }
+
+        [Fact]
+        public void HaveFullNameStartWith()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+            Assert.StartsWith("Sarah", sut.FullName); 
+        }
+        [Fact]
+        public void HaveFullNameEndsWith()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+            Assert.EndsWith("Smith", sut.FullName);
+        }
+
+        [Fact]
+        public void CheckFullName_IgnoreCaseAssertExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "SARAH";
+            sut.LastName = "SMITH";
+            Assert.Equal("Sarah Smith", sut.FullName, ignoreCase: true);
+        }
+
+        [Fact]
+        public void CheckFullName_SubstringAssertExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+            Assert.Contains("ah Sm", sut.FullName);
+        }
+
+        [Fact]
+        public void CheckFullNameWithTitleCase()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]", sut.FullName);
+        }
+
+        [Fact]
+        public void StartWithDefaultHealth()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            Assert.Equal(100, sut.Health);
+        }
+
+        [Fact]
+        public void StartWithDefaultHealth_NotEqualExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            Assert.NotEqual(0, sut.Health);
+        }
+
+        [Fact]
+        public void IncreaseHealthAfterSleeping()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.Sleep(); // Expect increase between 1 to 100 inclusive
+            //Assert.True(sut.Health>=101 && sut.Health<=200);
+            Assert.InRange(sut.Health, 101,200);
+        }
+
     }
 }
