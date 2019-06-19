f2aacf1c7dbe6880dd0aeb3554a099b45bc462b4
manish2981 <manish29malhotra@gmail.com>
2019-06-20 00:08:04 +1000

    Added First Test Method

---
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class PlayerCharacterShould
+    {
+        [Fact]
+        public void BeInexperiencedWhenNew()
+        {
+            PlayerCharacterShould sut = new PlayerCharacterShould();
+
+        }
+    }
+}
